1991e338f7672fb72902b51813cacc98e634da8e
Radded <mycooljames@hotmail.com>
2023-11-19 16:13:56 -0500

    fix: reinit as Nextron project with Tailwind CSS Nextron = Next.js + Electron

---
+++ renderer/pages/home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function HomePage() {
+	return (
+		<React.Fragment>
+			<Head>
+				<title>Welcome to the Furry Friend Favourable Mention System!</title>
+			</Head>
+			<div className=" h-screen">
+				<p>Hello world!</p>
+			</div>
+		</React.Fragment>
+	)
+}
