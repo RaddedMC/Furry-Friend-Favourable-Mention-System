665ecab7075b901d968aa95b2cd7edceadd26065
Radded <mycooljames@hotmail.com>
2023-12-06 23:33:12 -0500

    fix: Images crop properly, keys present

--- renderer/components/LandingPagePet.tsx
+++ renderer/components/LandingPagePet.tsx
@@ -7,7 +7,7 @@ type landingPetProps = {
 
 export default function LandingPagePet(props: landingPetProps) {
     return <div className='flex flex-col bg-black bg-opacity-70 rounded-3xl mx-2 min-w-fit'>
-        <Image className='rounded-3xl' width={200} height={200} src={props.imageUrl}/>
+        <Image className='rounded-3xl object-cover' width={200} height={200} src={props.imageUrl}/>
         <p className='text-lg text-center m-2'><b>{props.name}</b></p>
     </div>
 }
